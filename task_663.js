0a2edc980bb150c56f2ce4a4284796dfcd68fc68
Liliia <154988375+LiliiaGalkina@users.noreply.github.com>
2025-01-13 11:15:24 +0300

    Update task_663.js
    реализована проверка введенного значения

--- task_663.js
+++ task_663.js
@@ -1,13 +1,22 @@
 //Задание 6.6.3
 
 function makeArray(num) {
-  const arr = [];
-  for (let i = 0; i <= num; i++) {
-    arr.push(i);
-  }
-  return arr;
-}
+        const arr = [];
+        for (let i = 0; i <= num; i++) {
+            arr.push(i);
+        }
+        return arr;
+        }
 
-let digit = +prompt("Введите число:");
+  let digit = prompt("Введите число:");
 
-console.log(makeArray(digit));
+        if (digit === null) {
+            alert('Действие отменено пользователем.');
+        } else {digit = parseInt(digit);
+        }
+
+        if (isNaN(digit) || digit < 0) {
+            alert('Вы ввели некорректное или отрицательное число!');
+        } else {
+            console.log(makeArray(digit));
+        }
